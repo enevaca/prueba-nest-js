1a5879007a4d55b1d0f303892deadc5d172a3853
Noel Vaca <noel.vm.2501@gmail.com>
2021-09-26 21:53:08 +0000

    test 1

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,22 +8,25 @@ module.exports = {
       max_memory_restart: '256M',
       env_development: {
         PORT: 3000,
-        NODE_ENV: 'development',
+        // NODE_ENV: 'development',
       },
       env_production: {
         PORT: 3001,
-        NODE_ENV: 'production',
+        // NODE_ENV: 'production',
       },
     },
   ],
   deploy: {
-    dev: {
-      user: 'node',
-      host: '212.83.163.1',
-      repo: 'git@github.com:repo.git',
-      ref: 'origin/master',
+    development: {
+      user: 'enevaca',
+      host: ['192.168.1.20'],
+      repo: 'git@github.com:enevaca/prueba-nest-js.git',
+      ref: 'origin/main',
       path: '/var/www/development',
-      'post-deploy': 'pm2 startOrRestart ecosystem.config.js --env development',
+      'pre-deploy': 'git fetch --all',
+      'post-deploy': 'npm install'
+      //  'npm install -d && npm run build && pm2 reload ecosystem.config.js --env development',
+      // 'post-deploy': 'pm2 startOrRestart ecosystem.config.js --env development',
     },
     production: {
       user: 'ubuntu',
